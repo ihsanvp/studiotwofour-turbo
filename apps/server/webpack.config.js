38619bb6eca46a0f4f4b36843cc3cb8617eb4a24
ihsanvp <ihsanvp2738@gmail.com>
2022-07-28 11:17:33 +0530

    setup apps

---
+++ apps/server/webpack.config.js
@@ -0,0 +1,34 @@
+const WebpackShellPlugin = require("webpack-shell-plugin-next");
+const nodeExternals = require("webpack-node-externals");
+const path = require("path");
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  entry: "./server.ts",
+  mode: process.env.NODE_ENV,
+  target: "node",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ["ts-loader"],
+      },
+    ],
+  },
+  externals: [nodeExternals()],
+  plugins: [
+    new WebpackShellPlugin({
+      onBuildEnd: ["yarn run:dev"],
+    }),
+  ],
+  stats: {
+    errorDetails: true,
+  },
+};
